439be9eb0b32a417e517906f3c171ed5a0eaaeb6
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-08-09 15:40:59 -0400

    initial commit

---
+++ todo.sql
@@ -0,0 +1,18 @@
+CREATE TABLE todolist (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  details VARCHAR(10000),
+  priority INTEGER NOT NULL DEFAULT '1',
+  created_at NOT NULL,
+  completed_at TIMESTAMP
+);
+
+INSERT INTO todolist (title, details, priority, created_at) VALUES ('clean', 'take car to carwash', 3, NOW());
+INSERT INTO todolist (title, details, priority, created_at) VALUES ('work', 'leave early to not be late', 1, NOW());
+INSERT INTO todolist (title, details, priority, created_at, completed_at) VALUES ('brush', 'brush the cats fur', 2, NOW(), NOW());
+INSERT INTO todolist (title, priority, created_at) VALUES ('add to do', 5, NOW());
+
+SELECT title FROM todolist WHERE completed_at IS NULL;
+SELECT * FROM todolist WHERE priority > 1;
+UPDATE todolist SET completed_at = NOW() WHERE id = 1;
+DELETE FROM todolist WHERE title = 'work';
